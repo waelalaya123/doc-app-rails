7175b7ed0a4748e746d1b09151bc24ceab486d59
AbhiThakur <sambhav@devinventivesolutions.com>
2020-06-09 22:39:00 +0530

    corsfix

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,12 @@ Bundler.require(*Rails.groups)
 module RubyReact
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+         origins '*'
+         resource '*', :headers => :any, :methods => [:get, :post, :options]
+       end
+    end
     config.hosts << "5af80690.ngrok.io"
     config.load_defaults 6.0
 
